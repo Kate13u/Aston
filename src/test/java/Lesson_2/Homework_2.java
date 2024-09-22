e5653a75822484828cfe0aee95cf38b5bf9c9c9f
kate13u <udovenka.k@gmail.com>
2024-09-22 19:37:25 +0300

    HM_2

--- src/test/java/Lesson_2/Homework_2.java
+++ src/test/java/Lesson_2/Homework_2.java
@@ -1,12 +1,56 @@
 package Lesson_2;
 
 public class Homework_2 {
-    
-    public static void main(String[] args) { printThreeWords();}
 
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+        compareTwoNumbers();
+    }
     public static void printThreeWords() {
         System.out.println("Orange");
         System.out.println("Banana");
         System.out.println("Apple");
+    }
+    public static void checkSumSign() {
+        int a = 6;
+        int b = 2;
+        if (a + b >= 0) {
+            System.out.println("Сумма положительная");
+        }
+        if (a + b < 0) {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+    public static void printColor() {
+        int value = 78;
+        if (value <= 0) {
+            System.out.println("Красный");
+        }
+        if (value > 0 && value <= 100) {
+            System.out.println("Жёлтый");
+        }
+        if (value > 100) {
+            System.out.println("Зелёный");
+        }
+    }
+    public static void compareNumbers() {
+        int a = 89;
+        int b = 83;
+        if (a >= b) {
+            System.out.println("a >= b");
+        }
+        else System.out.println("a < b");
+    }
+    public static void compareTwoNumbers() {
+        int a = 10, b = 9;
+        int sum = a + b;
+        if (sum >= 10 && sum <= 20) {
+            System.out.println("True");
+        } else {
+            System.out.println("False");
+        }
     }
 }
